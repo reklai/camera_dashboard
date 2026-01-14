c8135d18b0c6b04ed41594713e98aabd579f7c38
reklai <official.reklai@gmail.com>
2026-01-14 13:04:03 +0900

    grid like layout works

--- main.py
+++ main.py
@@ -1,102 +1,28 @@
-from threading import Thread
-
-import cv2
-import datetime
-import argparse
-import imutils
-import time as time
-
-
-# used to record the time when we processed last frame
-prev_frame_time = 0
-
-# used to record the time at which we processed current frame
-new_frame_time = 0
-
-class FPS:
-	def __init__(self):
-		# store the start time, end time, and total number of frames
-		# that were examined between the start and end intervals
-		self._start = None
-		self._end = None
-		self._numFrames = 0
-	def start(self):
-		# start the timer
-		self._start = datetime.datetime.now()
-		return self
-	def stop(self):
-		# stop the timer
-		self._end = datetime.datetime.now()
-	def update(self):
-		# increment the total number of frames examined during the
-		# start and end intervals
-		self._numFrames += 1
-	def elapsed(self):
-		# return the total number of seconds between the start and
-		# end interval
-		return (self._end - self._start).total_seconds()
-	def fps(self):
-		# compute the (approximate) frames per second
-		return self._numFrames / self.elapsed()
-class WebcamVideoStream:
-	def __init__(self, src=0):
-		# initialize the video camera stream and read the first frame
-		# from the stream
-		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
-		# initialize the variable used to indicate if the thread should
-		# be stopped
-		self.stopped = False
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				return
-			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read()
-	def read(self):
-		# return the frame most recently read
-		return self.frame
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-	help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-	help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
-# created a *threaded* video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from webcam...")
-vs = WebcamVideoStream(src=4).start()
-fps = FPS().start()
-test = vs.stream.get(cv2.CAP_PROP_FPS)
-print(test)
-
-# loop over some frames...this time using the threaded stream
+# Codec Thread Translateion
+import threading, queue, cv2
+from queue import Empty
+# CORRECT USAGE
+cap = cv2.VideoCapture(5)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-	# check to see if the frame should be displayed to our screen
-	cv2.imshow("Frame", frame)
-	if cv2.waitKey(1) == ord('q'):
-		break
-	# update the FPS counter
-	fps.update()
-	# stop the timer and display FPS information
-	fps.stop()
-	new_frame_time = time.time()
-	current_fps = 1/(new_frame_time-prev_frame_time)
-	prev_frame_time = new_frame_time
-# do a bit of cleanup
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+ 
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
-vs.stop()
